23b39c679f349b4c2ab8c4f7a30c3b67cf37363c
ofcljaved <javedofficialme@gmail.com>
2024-11-15 22:35:14 +0530

    chore : init the account

--- anchor/programs/voting/src/lib.rs
+++ anchor/programs/voting/src/lib.rs
@@ -4,67 +4,55 @@ use anchor_lang::prelude::*;
 
 declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 
+pub const ANCHOR_DESCRIMINATOR_SIZE: usize = 8;
+
 #[program]
 pub mod voting {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseVoting>) -> Result<()> {
-    Ok(())
-  }
+    pub fn initialize_poll(
+        ctx: Context<InitializePoll>,
+        poll_id: u64,
+        description: String,
+        poll_start: u64,
+        poll_end: u64,
+    ) -> Result<()> {
+        let poll = &mut ctx.accounts.poll;
+        poll.id = poll_id;
+        poll.description = description;
+        poll.start_time = poll_start;
+        poll.end_time = poll_end;
 
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.voting.count = ctx.accounts.voting.count.checked_sub(1).unwrap();
-    Ok(())
-  }
-
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.voting.count = ctx.accounts.voting.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeVoting>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.voting.count = value.clone();
-    Ok(())
-  }
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
-pub struct InitializeVoting<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
+#[instruction(poll_id: u64)]
+pub struct InitializePoll<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
 
-  #[account(
-  init,
-  space = 8 + Voting::INIT_SPACE,
-  payer = payer
-  )]
-  pub voting: Account<'info, Voting>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseVoting<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
+    #[account(
+        init,
+        payer = signer,
+        space = ANCHOR_DESCRIMINATOR_SIZE + Poll::INIT_SPACE,
+        seeds = [poll_id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub poll: Account<'info, Poll>,
 
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub voting: Account<'info, Voting>,
+    pub system_program: Program<'info, System>,
 }
 
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub voting: Account<'info, Voting>,
-}
-
-#[account]
+#[account()]
 #[derive(InitSpace)]
-pub struct Voting {
-  count: u8,
+pub struct Poll {
+    pub id: u64,
+
+    #[max_len(280)]
+    pub description: String,
+    pub start_time: u64,
+    pub end_time: u64,
+    pub candidate_amount: u64,
 }
